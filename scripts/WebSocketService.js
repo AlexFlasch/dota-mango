a49d8beebb41f47b87aec3086ae978710f2bd606
Flascher <alexanderflasch@gmail.com>
2015-12-02 00:41:27 -0600

    Moving from a C# websocket server to using only a Node backend!
    Removed websocket related files,
    added some images,
    also removed some files that snuck in there from chromes downloads...
    (sometimes I wish it wouldn't remember the last folder you download to)
    also added apikey.json
    
    apikey.json will be ignored in future commits and currently contains an
    empty string. Will be replaced with my Steam WebAPI key in the future.

--- scripts/WebSocketService.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('mango');
-
-app.service('webSocketService', ['$http', function($http){
-	var server = new WebSocket("wss://" + __address__);
-
-	server.onopen = function(event) {
-		server.send('')
-	}
-}])
